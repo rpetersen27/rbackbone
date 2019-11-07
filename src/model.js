164db4ea3ea715e28a2c8d2210c8eb4185751497
Richard Petersen <richard@planet-petersen.de>
2019-11-07 21:01:19 +0100

    Introduce push reactive models with simple and computed properties

--- src/model.js
+++ src/model.js
@@ -1,10 +1,43 @@
-const { Model } = require('backbone');
-const { isFunction } = require('underscore');
+const { Model, Events } = require('backbone');
+const { each, extend, isFunction, isObject } = require('underscore');
+const store = require('./store');
+
+function makeDependency(attr, value) {
+    const model = this;
+    if (isFunction(value)) {
+        value.cache = undefined;
+        value.execute = function () {
+            const temp = store.dep;
+            store.dep = value;
+            value.stopListening();
+            const result = value.call(model);
+            store.dep = temp;
+            return result;
+        };
+        value.notify = function () {
+            const next = value.execute();
+            if (value.cache === next) return;
+            value.cache = next;
+            model.trigger('change:' + attr, model, next);
+        };
+        extend(value, Events);
+        value.cache = value.execute();
+    }
+}
 
 module.exports = Model.extend({
-    get: function (attr) {
-        const val = Model.prototype.get.call(this, attr);
-        if (isFunction(val)) return val.call(this);
+    get(attr) {
+        if (store.dep) {
+            store.dep.listenTo(this, `change:${attr}`, store.dep.notify);
+        }
+        let val = Model.prototype.get.call(this, attr);
+        if (isFunction(val)) return val.cache;
         return val;
+    },
+    set(attr, value, options) {
+        const result = Model.prototype.set.call(this, attr, value, options);
+        if (isObject(attr)) each(attr, (value, key) => makeDependency.call(this, key, value));
+        else makeDependency.call(this, attr, value);
+        return result;
     },
 });
--- test/model.test.js
+++ test/model.test.js
@@ -11,3 +11,37 @@ test('A model returns calculated values', () => {
     model.set('value', 10);
     expect(model.get('computed')).toBe(50);
 });
+
+test('A calculated value will be updated without being got', () => {
+    const model = new RModel({
+        value: 5,
+        computed: function () {
+            return this.get('value') * 5;
+        },
+    });
+    expect(model.attributes.computed.cache).toBe(25);
+    model.set('value', 10);
+    expect(model.attributes.computed.cache).toBe(50);
+});
+
+test('A calculated value will remove its dependencies if necessary', () => {
+    const model = new RModel({
+        a: true,
+        b: true,
+        computed: function () {
+            return this.get('a') && this.get('b');
+        },
+    });
+    expect(model.get('computed')).toBe(true);
+
+    const spy = jest.spyOn(model.attributes.computed, 'execute');
+    expect(spy.mock.calls.length).toBe(0);
+
+    model.set('a', false);
+    expect(model.get('computed')).toBe(false);
+    expect(spy.mock.calls.length).toBe(1);
+
+    model.set('b', false);
+    expect(model.get('computed')).toBe(false);
+    expect(spy.mock.calls.length).toBe(1);
+});
