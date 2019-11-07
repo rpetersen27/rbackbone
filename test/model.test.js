afa5043698eccd55a01c9cf8c4ab53203a11e34f
Richard Petersen <richard@planet-petersen.de>
2019-11-07 21:08:51 +0100

    Add moar tests

--- test/model.test.js
+++ test/model.test.js
@@ -45,3 +45,52 @@ test('A calculated value will remove its dependencies if necessary', () => {
     expect(model.get('computed')).toBe(false);
     expect(spy.mock.calls.length).toBe(1);
 });
+
+test('computed of computed property', () => {
+    const model = new RModel({
+        value: 5,
+        belowTen: function () {
+            return this.get('value') < 10;
+        },
+        invert: function () {
+            return !this.get('belowTen');
+        },
+    });
+
+    expect(model.get('belowTen')).toBe(true);
+    expect(model.get('invert')).toBe(false);
+
+    const spyBelowTen = jest.spyOn(model.attributes.belowTen, 'execute');
+    const spyInvert = jest.spyOn(model.attributes.invert, 'execute');
+
+    model.set('value', 7);
+
+    expect(model.get('belowTen')).toBe(true);
+    expect(model.get('invert')).toBe(false);
+    expect(spyBelowTen.mock.calls.length).toBe(1);
+    expect(spyInvert.mock.calls.length).toBe(0);
+
+    model.set('value', 12);
+
+    expect(model.get('belowTen')).toBe(false);
+    expect(model.get('invert')).toBe(true);
+    expect(spyBelowTen.mock.calls.length).toBe(2);
+    expect(spyInvert.mock.calls.length).toBe(1);
+});
+
+test('compute values from different models', () => {
+    const m1 = new RModel({ value: 3 });
+    const m2 = new RModel({ value: 7 });
+    const m3 = new RModel({
+        computed: function () {
+            return m1.get('value') * m2.get('value');
+        },
+    });
+    expect(m3.get('computed')).toBe(21);
+
+    m1.set('value', 4);
+    expect(m3.get('computed')).toBe(28);
+
+    m2.set('value', 5);
+    expect(m3.get('computed')).toBe(20);
+});
